0759da5590985d1581e53743dd94413f3750ed04
ruhamabek <ruhamauzubo@gmail.com>
2026-05-11 09:41:39 +0300

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,9 @@
 import * as fs from "fs";
+import { inflate } from 'node:zlib';
+import { promisify } from 'node:util';
+import * as path from 'node:path';
+
+const inflateAsync = promisify(inflate);
 
 const args = process.argv.slice(2);
 const command = args[0];
@@ -15,6 +20,18 @@ switch (command) {
     fs.writeFileSync(".git/HEAD", "ref: refs/heads/main\n");
     console.log("Initialized git directory");
     break;
+    case "cat-file":
+    const sha = args[2];
+    const firstPart = sha.slice(0, 2);
+    const secondPart = sha.slice(2);
+    const filePath = path.join('.git/objects', firstPart, secondPart);
+    const compressed = fs.readFileSync(filePath);
+    const original = await inflateAsync(compressed);
+    const decoded = original.toString();
+    const content = decoded.slice(decoded.indexOf("/0") + 1);
+    process.stdout.write(content);    
+    break;
   default:
     throw new Error(`Unknown command ${command}`);
+
 }
